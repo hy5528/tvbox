6c398e4286c23ed22c16916cc33157fc795bc844
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-10-11 21:17:21 +0800

    Update cms.sh

--- cms.sh
+++ cms.sh
@@ -7,6 +7,7 @@ sudo unzip maccms10.zip
 sudo chmod -R 777 /www/cms
 sudo docker run -it --name cms --restart=unless-stopped -p 3306:3306 -v /www/mysql:/var/lib/mysql -e MYSQL_DATABASE=cms -e MYSQL_USER=cms -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 --network=film-network yobasystems/alpine-mariadb:10.11
 sudo docker run -d --name film  --restart=unless-stopped --user $(id -u):$(id -g) -v /www/cms:/var/www/html  -p 80:80 -e ND_LOGLEVEL=info --network=filme-network shinsenter/phpfpm-apache:dev-php7.4 
+sudo chmod -R 777 /www
 cd ~
 echo "Everything is ok!"
 echo "Open the website: http://`hostname -I|awk '{print $1}'`"
