e2ec5025e78bfdbcc8ca88adf66c019ac5b12999
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-10-11 21:25:13 +0800

    Update cms.sh

--- cms.sh
+++ cms.sh
@@ -5,8 +5,8 @@ sudo mkdir -p /www/cms && cd /www/cms
 wget https://gh.con.sh/https://raw.githubusercontent.com/magicblack/maccms_down/master/maccms10.zip
 sudo unzip maccms10.zip
 sudo chmod -R 777 /www/cms
-sudo docker run -it --name cms --restart=unless-stopped -p 3306:3306 -v /www/mysql:/var/lib/mysql -e MYSQL_DATABASE=cms -e MYSQL_USER=cms -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 --network=film-network yobasystems/alpine-mariadb:10.11
-sudo docker run -d --name film  --restart=unless-stopped --user $(id -u):$(id -g) -v /www/cms:/var/www/html  -p 80:80 -e ND_LOGLEVEL=info --network=filme-network shinsenter/phpfpm-apache:dev-php7.4 
+sudo docker run -it --name cms --restart=unless-stopped -p 3306:3306 -v /www/mysql:/var/lib/mysql -e MYSQL_DATABASE=cms -e MYSQL_USER=cms -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 --network=1panel-network --ip=172.18.0.3 yobasystems/alpine-mariadb:10.11
+sudo docker run -d --name film  --restart=unless-stopped --user $(id -u):$(id -g) -v /www/cms:/var/www/html  -p 80:80 -e ND_LOGLEVEL=info --network=1panel-network --ip=172.18.0.2 shinsenter/phpfpm-apache:dev-php7.4 
 sudo chmod -R 777 /www
 cd ~
 echo "Everything is ok!"
