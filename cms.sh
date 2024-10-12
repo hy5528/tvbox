5b802b9c672ec2a026a12838a060df6863f5baa2
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-10-12 09:28:29 +0800

    Update cms.sh

--- cms.sh
+++ cms.sh
@@ -12,6 +12,7 @@ docker network create --subnet 172.19.0.0/16 --gateway 172.19.1.1 --driver bridg
 sudo docker run -d --name film  --restart=always --user $(id -u):$(id -g) -v /www/cms:/var/www/html  -p 80:80 -e ND_LOGLEVEL=info --network=film_network --ip=172.19.0.2 shinsenter/phpfpm-apache:dev-php7.4 
 sudo docker run -it --name cms --restart=always -p 3306:3306 -v /www/mysql:/var/lib/mysql -e MYSQL_DATABASE=cms -e MYSQL_USER=cms -e MYSQL_PASSWORD=123456 -e MYSQL_ROOT_PASSWORD=123456 --network=film_network --ip=172.19.0.3 yobasystems/alpine-mariadb:10.11
 sudo chmod -R 777 /www
+sudo chmod -R 777 /www/cms
 cd ~
 echo "Everything is ok!"
 echo "Open the website: http://`hostname -I|awk '{print $1}'`"
