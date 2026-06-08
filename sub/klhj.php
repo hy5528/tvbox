72fcb73cb7842d4017607048b73df39f14eccc21
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-06-08 22:20:13 +0800

    Create klhj.php

---
+++ sub/klhj.php
@@ -0,0 +1,20 @@
+{
+    "class": [
+        {
+            "type_name": "电影天堂克隆",
+            "type_id": "0"
+        },
+        {
+            "type_name": "金鹰采集克隆",
+            "type_id": "1"
+        },
+        {
+            "type_name": "索尼采集克隆",
+            "type_id": "2"
+        },
+        {
+            "type_name": "暴风采集克隆",
+            "type_id": "3"
+        }
+    ]
+}
