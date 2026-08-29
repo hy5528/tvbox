7783083e306a55a79353ac6863744c6f7fa18168
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-08-29 12:12:06 +0800

    Update BLZB.py

--- py/BLZB.py
+++ py/BLZB.py
@@ -122,7 +122,6 @@ class Spider(Spider):
         result = {}
         result = {"class": [{"type_id": "舞", "type_name": "舞蹈"},
                             {"type_id": "音乐", "type_name": "音乐"},
-                            {"type_id": "2026年热们歌曲超清", "type_name": "热榜"},
                             {"type_id": "歌曲", "type_name": "歌曲"},
                             {"type_id": "粤语歌曲超清", "type_name": "粤语"},
                             {"type_id": "手游", "type_name": "手游"},
